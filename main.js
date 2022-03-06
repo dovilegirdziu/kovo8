1738a8e152883d6e28d734bde08b90acf6614c5e
dovilegirdziu <97856821+dgstudiolt@users.noreply.github.com>
2022-03-07 00:07:16 +0200

    better fit for phone

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.querySelector('body').addEventListener('click', changeBg);
 
-bgVideos = ["./vid/INNORECORDSPhotoVideos.mp4", "./vid/KarolinaGrabowskaBluringRose.mp4", "./vid/pixabayBird.mp4", "./vid/Pixabay-CherryBlossoms.mp4", "./vid/pixabayLotus.mp4", "./vid/pixabayRose.mp4"]
+bgVideos = ["./vid/INNORECORDSPhotoVideos.mp4", "./vid/KarolinaGrabowskaBluringRose.mp4", "./vid/Pixabay-CherryBlossoms.mp4", "./vid/pixabayLotus.mp4", "./vid/pixabayRose.mp4"]
 
 function getRandomVideo() {
     return bgVideos[Math.floor(Math.random()*bgVideos.length)];
